eac67c1f58427ae5f08a6c56b66a3795dac1b168
rafiarrafif <rafiarrafif05@gmail.com>
2025-06-19 22:13:15 +0700

    add project structure documentation

--- src/modules/auth/services/loginFromSystem.service.ts
+++ src/modules/auth/services/loginFromSystem.service.ts
@@ -1,1 +1,9 @@
-export const loginFromSystemService = (userId: string) => {};
+import { UserHeaderInformation } from "../../../helpers/http/userHeader/getUserHeaderInformation/types";
+import { createUserSessionService } from "../../userSession/services/createUserSession.service";
+
+export const loginFromSystemService = (
+  userId: string,
+  userHeaderInfo: UserHeaderInformation
+) => {
+  // const userSession = await createUserSessionService()
+};
