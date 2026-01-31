83792848edf429c1465ba4332b1f2384dd3bb9fc
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-01-31 18:41:09 +0700

    💚 ci: fix auto script causing CI failure

--- scripts/create-dummy-system-account.ts
+++ scripts/create-dummy-system-account.ts
@@ -1,10 +1,16 @@
 import { generateUUIDv7 } from "../src/helpers/databases/uuidv7";
 import { createFile } from "../src/helpers/files/createFile";
 
-export const createDummySystemAccount = async () => {
-  await createFile(`export const SystemAccountId = "${generateUUIDv7()}";`, {
-    fileName: "system.ts",
-    targetDir: "src/config/account",
-    overwriteIfExists: true,
-  });
+const createDummySystemAccount = async () => {
+  const file = await createFile(
+    `export const SystemAccountId = "${generateUUIDv7()}";`,
+    {
+      fileName: "system.ts",
+      targetDir: "src/config/account",
+      overwriteIfExists: true,
+    },
+  );
+  console.log(`Dummy system account created with id in file: ${file}`);
 };
+
+createDummySystemAccount();
