3122f340931f1f32714377ab22ee4560b633b180
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-02-17 21:33:59 +0700

    🛂 security: fix auth token validation flow

--- src/modules/auth/services/http/tokenValidation.service.ts
+++ src/modules/auth/services/http/tokenValidation.service.ts
@@ -1,9 +1,11 @@
+import { AppError } from "../../../../helpers/error/instances/app";
 import { ErrorForwarder } from "../../../../helpers/error/instances/forwarder";
 import { jwtDecode } from "../../../../helpers/http/jwt/decode";
 
 export const tokenValidationService = (payload: string) => {
   try {
-    if (!payload) return null;
+    if (!payload || payload.trim() === "")
+      throw new AppError(401, "Unauthorized: No token provided");
     const decoded = jwtDecode(payload);
     return decoded;
   } catch (error) {
