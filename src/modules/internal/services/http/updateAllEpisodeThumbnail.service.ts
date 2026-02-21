1038ad068f023ca9def91c719031f2140548f11e
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-02-21 19:53:17 +0700

    🗑️ chore: remove repository from service

--- src/modules/internal/services/http/updateAllEpisodeThumbnail.service.ts
+++ src/modules/internal/services/http/updateAllEpisodeThumbnail.service.ts
@@ -1,35 +1,14 @@
 import { AppError } from "../../../../helpers/error/instances/app";
 import { ErrorForwarder } from "../../../../helpers/error/instances/forwarder";
-import { updateEpisodeRepository } from "../../../episode/repositories/PUT/updateEpisode.repository";
-import { getAllEpisodeWithThumbnailLinkRepository } from "../../../episode/repositories/GET/getAllEpisodeWithThumbnailLink.repository";
 
 export const updateAllEpisodeThumbnailService = async (
-  serviceReferenceId: string,
+  serviceReferenceId?: string,
 ) => {
   try {
     if (!serviceReferenceId)
       throw new AppError(400, "Service Reference ID is required.");
 
-    const episodesData = await getAllEpisodeWithThumbnailLinkRepository(
-      serviceReferenceId,
-    );
-
-    let updatedThumbnailsCount = 0;
-    for (const episode of episodesData) {
-      if (episode.videos.length === 0) continue;
-      await updateEpisodeRepository({
-        id: episode.id,
-        pictureThumbnail:
-          episode.videos[0].service.endpointThumbnail?.replace(
-            ":code:",
-            episode.videos[0].code,
-          ) || null,
-      });
-
-      updatedThumbnailsCount++;
-    }
-
-    return updatedThumbnailsCount;
+    return serviceReferenceId;
   } catch (error) {
     ErrorForwarder(error);
   }
