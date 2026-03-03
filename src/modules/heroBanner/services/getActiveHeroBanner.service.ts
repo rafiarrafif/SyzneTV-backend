5a7f9bbebe54e52995de5d0119fc4be297818aac
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-03-03 21:49:32 +0700

    ⚡ perf: use Redis for faster system preference check

--- src/modules/heroBanner/services/getActiveHeroBanner.service.ts
+++ src/modules/heroBanner/services/getActiveHeroBanner.service.ts
@@ -1,10 +1,14 @@
 import { AppError } from "../../../helpers/error/instances/app";
 import { ErrorForwarder } from "../../../helpers/error/instances/forwarder";
+import { findSystemPreferenceService } from "../../systemPreference/services/internal/findSystemPreference.service";
 import { findAllActiveHeroBannerRepository } from "../repositories/GET/findAllActiveHeroBanner.repository";
 
 export const getActiveHeroBannerService = async () => {
   try {
-    const isHeroBannerEnabled = process.env.ENABLE_HERO_BANNER === "true";
+    const isHeroBannerEnabled = await findSystemPreferenceService(
+      "HERO_BANNER_ENABLED",
+      "boolean",
+    );
     if (!isHeroBannerEnabled)
       throw new AppError(403, "Hero Banner is disabled");
 
