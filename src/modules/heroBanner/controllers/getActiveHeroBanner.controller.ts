5cebd200c441a182c81481abcff6e096efb02481
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-03-30 19:03:37 +0700

    👔 feat: support user collection check on hero banner

--- src/modules/heroBanner/controllers/getActiveHeroBanner.controller.ts
+++ src/modules/heroBanner/controllers/getActiveHeroBanner.controller.ts
@@ -1,17 +1,16 @@
-import { Context } from "elysia";
+import { Context, Static } from "elysia";
 import { mainErrorHandler } from "../../../helpers/error/handler";
 import { returnReadResponse } from "../../../helpers/callback/httpResponse";
 import { getActiveHeroBannerService } from "../services/getActiveHeroBanner.service";
+import { getActiveHeroBannerSchema } from "../schemas/getActiveHeroBanner.schema";
 
-export const getActiveHeroBannerController = async (ctx: Context) => {
+export const getActiveHeroBannerController = async (ctx: {
+  set: Context["set"];
+  header: Static<typeof getActiveHeroBannerSchema.headers>;
+}) => {
   try {
-    const response = await getActiveHeroBannerService();
-    return returnReadResponse(
-      ctx.set,
-      200,
-      "Active hero banners fetched successfully",
-      response,
-    );
+    const response = await getActiveHeroBannerService({ cookie: ctx.header?.cookie });
+    return returnReadResponse(ctx.set, 200, "Active hero banners fetched successfully", response);
   } catch (error) {
     return mainErrorHandler(ctx.set, error);
   }
