b27479cd3e19c6e5afffb1d140d9a72e18cb6b2e
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-04-01 23:38:32 +0700

    🐛 fix: resolve schema type error in getAllMedia module

--- src/modules/media/controllers/getAllMedia.controller.ts
+++ src/modules/media/controllers/getAllMedia.controller.ts
@@ -1,19 +1,16 @@
-import { Context } from "elysia";
+import { Context, Static } from "elysia";
 import { mainErrorHandler } from "../../../helpers/error/handler";
 import { getAllMediaService } from "../services/http/getAllMedia.service";
 import { returnReadResponse } from "../../../helpers/callback/httpResponse";
+import { getAllMediaSchema } from "../schemas/getAllMedia.schema";
 
-export const getAllMediaController = async (
-  ctx: Context & { query: { page: string } },
-) => {
+export const getAllMediaController = async (ctx: {
+  set: Context["set"];
+  query: Static<typeof getAllMediaSchema.query>;
+}) => {
   try {
     const mediaData = await getAllMediaService(ctx.query.page);
-    return returnReadResponse(
-      ctx.set,
-      200,
-      "Media fetched successfully",
-      mediaData,
-    );
+    return returnReadResponse(ctx.set, 200, "Media fetched successfully", mediaData);
   } catch (error) {
     return mainErrorHandler(ctx.set, error);
   }
