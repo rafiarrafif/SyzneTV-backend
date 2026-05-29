e92d99662172c99efe9b3f6503b4ac240794c48c
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-05-29 14:07:40 +0700

    ♻️ refactor: update hero banner code for new schema

--- src/modules/heroBanner/services/getActiveHeroBanner.service.ts
+++ src/modules/heroBanner/services/getActiveHeroBanner.service.ts
@@ -1,44 +1,14 @@
 import { AppError } from "../../../helpers/error/instances/app";
 import { ErrorForwarder } from "../../../helpers/error/instances/forwarder";
-import { tokenValidationService } from "../../auth/services/http/tokenValidation.service";
-import { findSystemPreferenceService } from "../../systemPreference/services/internal/findSystemPreference.service";
-import { findAllActiveHeroBannerRepository } from "../repositories/GET/findAllActiveHeroBanner.repository";
+import { showHeroBannerToHomePageRepository } from "../repositories/READ/showHeroBannerToHomePage.repository";
 
 export const getActiveHeroBannerService = async ({ cookie }: { cookie?: string }) => {
   try {
-    const userId = cookie ? (await tokenValidationService(cookie)).user.id : undefined;
+    const isHeroBannerEnabled = process.env.ENABLE_HERO_BANNER === "true";
 
-    // Check if Hero Banner is enabled in system preferences
-    const isHeroBannerEnabled = await findSystemPreferenceService("HERO_BANNER_ENABLED", "boolean");
-    if (!isHeroBannerEnabled) throw new AppError(403, "Hero Banner is disabled");
+    if (!isHeroBannerEnabled) throw new AppError(403, "Hero banner feature is disabled");
 
-    // Don’t implement caching just yet; implement collection caching first, then implement banner caching.
-    // Please note that currently, a database query is still required to check the collections.
-    // // Try to get active banners from Redis cache
-    // const cachedBanners = await redis.get(`${redisKey.filter((key) => key.name === "HERO_BANNER")[0].key}`);
-    // if (cachedBanners) return JSON.parse(cachedBanners);
-
-    // If not in cache, fetch from database and cache the result
-    const activeBanners = await findAllActiveHeroBannerRepository(userId);
-    const constructedBanners = activeBanners.map((banner) => ({
-      id: banner.media.id,
-      title: banner.media.title,
-      slug: banner.media.slug,
-      imageUrl: banner.imageUrl || banner.media.pictureLarge,
-      synopsis: banner.media.synopsis,
-      genres: banner.media.genres.map((genre) => ({
-        slug: genre.slug,
-        name: genre.name,
-      })),
-      isInCollection: banner.media._count.inCollections > 0,
-    }));
-
-    // await redis.set(
-    //   `${redisKey.filter((key) => key.name === "HERO_BANNER")[0].key}`,
-    //   JSON.stringify(constructedBanners),
-    // );
-
-    return constructedBanners;
+    return await showHeroBannerToHomePageRepository();
   } catch (error) {
     ErrorForwarder(error);
   }
