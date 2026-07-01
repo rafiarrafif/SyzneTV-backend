502e7b10c6a3054ec86f4960fea8287b90040b31
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-07-01 10:25:29 +0700

    db: updated character schema

--- prisma/migrations/20260530045455_make_embed_url_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[embed_url]` on the table `media_trailers` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "media_trailers_embed_url_key" ON "media_trailers"("embed_url");
--- prisma/migrations/20260530050918_make_cc_as_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[code]` on the table `countries` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "countries_code_key" ON "countries"("code");
