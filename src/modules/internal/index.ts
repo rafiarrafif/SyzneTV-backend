a46b465409c1cc695e1bda57456afbfa7500abeb
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-07-02 22:40:33 +0700

    wip: reconstruct bulk insert video service

--- src/modules/internal/index.ts
+++ src/modules/internal/index.ts
@@ -1,27 +1,24 @@
 import Elysia from "elysia";
-import { bulkInsertEpisodeController } from "./controllers/bulkInsertEpisode.controller";
-import { bulkInsertMediaController } from "./controllers/bulkInsertMedia.controller";
-import { createVideoServiceInternalController } from "./controllers/createVideoService.controller";
-import { bulkInsertVideoController } from "./controllers/bulkInsertVideo.controller";
-import { updateAllEpisodeThumbnailController } from "./controllers/updateAllEpisodeThumbnail.controller";
-import { purgeUnusedSessionController } from "./controllers/purgeUnusedSession.controller";
-import { createHeroBannerController } from "./controllers/createHeroBanner.controller";
-import { bulkInsertMediaSchema } from "./schemas/bulkInsertMedia.schema";
-import { bulkInsertEpisodeSchema } from "./schemas/bulkInsertEpisode.schema";
-import { updateAllEpisodeThumbnailSchema } from "./schemas/updateAllEpisodeThumbnail.schema";
-import { bulkInsertVideoSchema } from "./schemas/bulkInsertVideo.schema";
-import { createVideoServiceInternalSchema } from "./schemas/createVideoServiceInternal.schema";
-import { purgeUnusedSessionSchema } from "./schemas/purgeUnusedSession.schema";
-import { createHeroBannerSchema } from "./schemas/createHeroBanner.schema";
+import {bulkInsertEpisodeController} from "./controllers/bulkInsertEpisode.controller";
+import {bulkInsertMediaController} from "./controllers/bulkInsertMedia.controller";
+import {createVideoServiceInternalController} from "./controllers/createVideoService.controller";
+import {bulkInsertVideoController} from "./controllers/bulkInsertVideo.controller";
+import {purgeUnusedSessionController} from "./controllers/purgeUnusedSession.controller";
+import {createHeroBannerController} from "./controllers/createHeroBanner.controller";
+import {bulkInsertMediaSchema} from "./schemas/bulkInsertMedia.schema";
+import {bulkInsertEpisodeSchema} from "./schemas/bulkInsertEpisode.schema";
+import {bulkInsertVideoSchema} from "./schemas/bulkInsertVideo.schema";
+import {createVideoServiceInternalSchema} from "./schemas/createVideoServiceInternal.schema";
+import {purgeUnusedSessionSchema} from "./schemas/purgeUnusedSession.schema";
+import {createHeroBannerSchema} from "./schemas/createHeroBanner.schema";
 
 export const internalModule = new Elysia({
-  prefix: "/internal",
-  tags: ["Internal"],
+    prefix: "/internal",
+    tags: ["Internal"],
 })
-  .post("/media/bulk-insert", bulkInsertMediaController, bulkInsertMediaSchema)
-  .post("/episode/bulk-insert", bulkInsertEpisodeController, bulkInsertEpisodeSchema)
-  .put("/episode/update-thumbnails", updateAllEpisodeThumbnailController, updateAllEpisodeThumbnailSchema)
-  .post("/video/bulk-insert", bulkInsertVideoController, bulkInsertVideoSchema)
-  .post("/video-service", createVideoServiceInternalController, createVideoServiceInternalSchema)
-  .post("/user-session/purge-unused", purgeUnusedSessionController, purgeUnusedSessionSchema)
-  .post("/hero-banner", createHeroBannerController, createHeroBannerSchema);
+    .post("/media/bulk-insert", bulkInsertMediaController, bulkInsertMediaSchema)
+    .post("/episode/bulk-insert", bulkInsertEpisodeController, bulkInsertEpisodeSchema)
+    .post("/video/bulk-insert", bulkInsertVideoController, bulkInsertVideoSchema)
+    .post("/video-service", createVideoServiceInternalController, createVideoServiceInternalSchema)
+    .post("/user-session/purge-unused", purgeUnusedSessionController, purgeUnusedSessionSchema)
+    .post("/hero-banner", createHeroBannerController, createHeroBannerSchema);
--- src/modules/internal/services/http/bulkInsertVideo.service.ts
+++ src/modules/internal/services/http/bulkInsertVideo.service.ts
@@ -1,35 +1,28 @@
-import { SystemAccountId } from "../../../../config/account/system";
-import { ErrorForwarder } from "../../../../helpers/error/instances/forwarder";
-import { findEpisodeWithMediaIdRepository } from "../../repositories/findEpisodeWithMediaId.repository";
-import { bulkInsertVideoRepository } from "../../repositories/bulkInsertVideo.repository";
-import { Static } from "elysia";
-import { bulkInsertVideoSchema } from "../../schemas/bulkInsertVideo.schema";
+import {SystemAccountId} from "../../../../config/account/system";
+import {ErrorForwarder} from "../../../../helpers/error/instances/forwarder";
+import {bulkInsertVideoRepository} from "../../repositories/bulkInsertVideo.repository";
+import {Static} from "elysia";
+import {bulkInsertVideoSchema} from "../../schemas/bulkInsertVideo.schema";
+import {Prisma} from "@prisma/client";
 
 export const bulkInsertVideoService = async (body: Static<typeof bulkInsertVideoSchema.body>) => {
-  try {
-    const insertedVideos: string[] = [];
-    for (const episodeData of body.data) {
-      const episodeId = await findEpisodeWithMediaIdRepository({
-        media: body.media_id,
-        episode: episodeData.episode,
-      });
-
-      for (const videoData of episodeData.videos) {
-        const insertedVideo = await bulkInsertVideoRepository({
-          pendingUpload: false,
-          episodeId: episodeId.id,
-          serviceId: videoData.service_id,
-          videoCode: videoData.video_code,
-          thumbnailCode: videoData.thumbnail_code,
-          uploadedBy: SystemAccountId,
-        });
-
-        insertedVideos.push(insertedVideo.id);
-      }
+    try {
+        const constructedInput: Prisma.VideoCreateManyInput[] = body.data.flatMap((d) => (
+            d.videos.flatMap((v) => (
+                {
+                    created_by_id: SystemAccountId,
+                    media_id: body.media_id,
+                    episode_number: d.episode,
+                    video_service_id: v.service_id,
+                    video_code: v.video_code,
+                    short_code: v.short_code,
+                    thumbnail_code: v.thumbnail_code,
+                    download_code: v.download_code
+                }
+            ))
+        ));
+        return constructedInput
+    } catch (error) {
+        ErrorForwarder(error);
     }
-
-    return insertedVideos;
-  } catch (error) {
-    ErrorForwarder(error);
-  }
 };
