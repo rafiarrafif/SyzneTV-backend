dc0d44da2bb52a846a6c24f35d12127f49ad8298
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-07-04 21:00:00 +0700

    refactor: create a new video provider module

--- src/modules/internal/repositories/createVideoService.repository.ts
+++ src/modules/internal/repositories/createVideoService.repository.ts
@@ -1,23 +1,23 @@
-import { Prisma } from "@prisma/client";
-import { AppError } from "../../../helpers/error/instances/app";
-import { prisma } from "../../../utils/databases/prisma/connection";
-import { generateUUIDv7 } from "../../../helpers/databases/uuidv7";
+import {Prisma} from "@prisma/client";
+import {AppError} from "../../../helpers/error/instances/app";
+import {prisma} from "../../../utils/databases/prisma/connection";
+import {generateUUIDv7} from "../../../helpers/databases/uuidv7";
 
 export const createVideoServiceInternalRepository = async (
-  payload: Omit<Prisma.VideoServiceUncheckedCreateInput, "id">,
+    payload: Prisma.VideoServiceCreateInput,
 ) => {
-  try {
-    return await prisma.videoService.upsert({
-      where: {
-        name: payload.name,
-      },
-      create: {
-        id: generateUUIDv7(),
-        ...payload,
-      },
-      update: payload,
-    });
-  } catch (error) {
-    throw new AppError(500, "Failed to create video service", error);
-  }
+    try {
+        return await prisma.videoService.upsert({
+            where: {
+                slug_resolution: {
+                    slug: payload.name.toLowerCase(),
+                    resolution: payload.resolution,
+                }
+            },
+            create: payload,
+            update: payload,
+        });
+    } catch (error) {
+        throw new AppError(500, "Failed to create video service", error);
+    }
 };
